9c199bf6fe1790a1ce069eafcaf5982b044af23b
KABOTE LUPAYA Destin <84081391+Destin-Lupaya@users.noreply.github.com>
2023-04-11 16:12:59 +0200

    report

---
+++ rootsandshoots/Etat_Rapport_Term_Sec_Femmes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rootsandshoots
+{
+    public partial class Etat_Rapport_Term_Sec_Femmes : UserControl
+    {
+        public Etat_Rapport_Term_Sec_Femmes()
+        {
+            InitializeComponent();
+        }
+    }
+}
